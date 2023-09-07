7f0563a0f82c3a6e9a3d79ece170d3edd0406076
Lim <limsangkyu0219@gmail.com>
2023-09-07 18:21:40 +0900

    #19 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    나머지 필요 기능은 추후 추가 예정

---
+++ src/main/java/com/example/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,17 @@
+package com.example.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link com.example.projectboard.domain.ArticleComment}
+ */
+public record ArticleCommentDto(LocalDateTime createdAt,
+                                String createdBy,
+                                LocalDateTime modifiedAt,
+                                String modifiedBy,
+                                String content
+) {
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
