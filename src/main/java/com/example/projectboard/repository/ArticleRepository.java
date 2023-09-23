3b1b6a1bf0e9a318469f9bc8c909f7e578287f65
Lim <limsangkyu0219@gmail.com>
2023-09-23 18:35:38 +0900

    #31 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를
    정리해서 보여주는 기능을 넣기 위해,
    해시태그만 뽑아서 unique 하게 보여주는 쿼리를
    querydsl 로 작성함
    그 외 서비스 코드, 컨트롤러, 관련 테스트 작성

--- src/main/java/com/example/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/example/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.example.projectboard.repository;
 
 import com.example.projectboard.domain.Article;
 import com.example.projectboard.domain.QArticle;
+import com.example.projectboard.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.domain.Page;
@@ -16,6 +17,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>,
+        ArticleRepositoryCustom,
         QuerydslBinderCustomizer<QArticle> {
 
     Page<Article> findByTitleContaining(String title, Pageable pageable);
