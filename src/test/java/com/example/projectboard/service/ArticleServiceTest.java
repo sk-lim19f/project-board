61fe26d03995576f648acbc31a8ebd722983d168
Lim <limsangkyu0219@gmail.com>
2023-09-24 15:10:23 +0900

    #34 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/test/java/com/example/projectboard/service/ArticleServiceTest.java
+++ src/test/java/com/example/projectboard/service/ArticleServiceTest.java
@@ -261,7 +261,6 @@ class ArticleServiceTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "Lim",
                 "password",
                 "Lim@mail.com",
